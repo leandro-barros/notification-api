ec5cc85c9ba0e33ca817684d91c05d7e6340fe35
Leandro Barros <leandroedbarros@gmail.com>
2021-10-13 21:05:41 -0300

    First commit

---
+++ src/test/java/com/softmei/notification/NotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmei.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
