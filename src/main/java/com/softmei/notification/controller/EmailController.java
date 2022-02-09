806d722a63c784abe882b47d666f44c7ebc3cac1
Leandro Barros <leobarros.evangelista@gmail.com>
2022-02-08 21:44:54 -0300

    Implementing LOG slf4j in project

--- src/main/java/com/softmei/notification/controller/EmailController.java
+++ src/main/java/com/softmei/notification/controller/EmailController.java
@@ -2,6 +2,8 @@ package com.softmei.notification.controller;
 
 import com.softmei.notification.dto.request.EmailRequestDto;
 import com.softmei.notification.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class EmailController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EmailController.class);
+
     @Autowired
     private EmailService emailService;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("send")
     public void sendEmail(@RequestBody EmailRequestDto emailRequestDto) {
+        LOG.info("recebida requisição de envio de e-mail.", emailRequestDto);
         emailService.sendEmail(emailRequestDto);
     }
 
--- src/main/java/com/softmei/notification/service/EmailService.java
+++ src/main/java/com/softmei/notification/service/EmailService.java
@@ -1,6 +1,8 @@
 package com.softmei.notification.service;
 
 import com.softmei.notification.dto.request.EmailRequestDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -15,6 +17,8 @@ import java.util.Locale;
 
 @Service
 public class EmailService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EmailService.class);
 
     @Autowired
     private JavaMailSender mailSender;
@@ -32,14 +36,16 @@ public class EmailService {
             helper.setTo(emailsTo.toArray(new String[emailsTo.size()]));
             helper.setSubject(subject);
             helper.setText(message, true);
-
+            LOG.info("Enviando e-mail");
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
+            LOG.info("Erro no envio de e-mail", e);
             throw new RuntimeException("Erro no envio de e-mail!", e);
         }
     }
 
     public void sendEmail(EmailRequestDto emailRequestDto) {
+        LOG.info("Criando template do e-mail");
         Context context = new Context(new Locale("pt", "BR"));
         emailRequestDto.getVariables().entrySet().forEach(e -> context.setVariable(e.getKey(), e.getValue()));
         String message = thymeleaf.process(emailRequestDto.getText(), context);
